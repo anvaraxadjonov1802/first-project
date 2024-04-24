60f599145928a9e04cbbb5114d06379a57f8a0d9
anvaraxadjonov1802 <axadjonov123@gmail.com>
2024-04-25 00:43:09 +0500

    commit

--- my_projects/blog_sites/articles/models.py
+++ my_projects/blog_sites/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Maqola(models.Model):
+    title = models.CharField(max_length = 255)
+    author = models.CharField(max_length = 255, null =True, blank = True)
+    description = models.TextField()
+    created_at = models.TimeField(auto_now_add = True)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+    
